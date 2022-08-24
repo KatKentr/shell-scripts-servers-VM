1d6b9577dae3e00b42fdfb8c327d772cb9db2655
KatKentr <kkentroti@gmail.com>
2022-08-24 16:07:23 +0300

    service as user input

--- manage_services/start_service.sh
+++ manage_services/start_service.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
+service="$1"
+
 #check if service is running
 
 #Remember: 0 is true in bash!
-if  pidof apache2 > /dev/null
+if  pidof ${service} > /dev/null
 then
-   echo "service is already running"
+   echo "$service is already running"
 else
    #start a service
-   echo "1234" | sudo -S systemctl start apache2
+   echo "1234" | sudo -S systemctl start ${service}
    sleep 3
 fi
 
 #check if service is running
-systemctl status apache2 | grep 'active (running)' > /dev/null 2>1%
+systemctl status ${service} | grep 'active (running)' > /dev/null 2>1%
 
 #grep returns 0 if pattern is found(true) and 1 if the pattern not found
 if [ $? != 0 ]
 then
-        echo "Service is not running"
+        echo "$service is not running"
         exit
 fi
 
-echo "service is running"
+echo "$service is running"
 
 #system reboots. However password is still required to login
 
