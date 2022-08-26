567e9a3f6dac44fa20c2709181ccf91d588a3c3a
KatKentr <kkentroti@gmail.com>
2022-08-26 16:31:24 +0300

    option to run and terminate shell script within a shell script tested

--- manage_services/start_service.sh
+++ manage_services/start_service.sh
@@ -4,6 +4,8 @@
 
 service="$1"
 
+now=$(date +"%Y.%m.%d-%H.%M.%S")
+
 #check if service is running
 
 #Remember: 0 is true in bash!
@@ -30,6 +32,25 @@ echo "$service is running"
 
 #change status of the variable to notify client,  (an attempt to work with a shared variable in a shared file for communication between the two VMs)
 sed -i 's/serverOn=.*/serverOn=1/' /media/sf_shared_between-VMs/notify_status.sh
+
+#source ~/Desktop/shell_scripts_VM_Servers/cpu_memory_stats/top_mulProcesses_stats.sh ${service} output_top_mulProcesses_stats_${now}.csv
+
+#start the script in the background and give back control to the script start_service.sh
+bash ~/Desktop/shell_scripts_VM_Servers/cpu_memory_stats/top_mulProcesses_stats.sh ${service} output_top_mulProcesses_stats_${now}.csv &
+
+pidIs=$!
+echo $pidIs
+
+sleep 3s
+
+# another way to kill the process, it worked by executing with bash.Thought: Could it work with source?
+#kill -SIGTERM $pidIs          # Give the process a chance to shut down
+#kill -SIGKILL $pidIs         
+
+# to get a process name for a shell script it needs to be executed with bash, otherwise command name is bash
+pkill -f top_mulProcesses_stats.sh
+
+echo "memory monitoring terminated"
 
 
 #system reboots. However password is still required to login
