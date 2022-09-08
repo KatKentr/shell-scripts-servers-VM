e12c7ba8435dde6917787c9454814db0387e9df8
KatKentr <kkentroti@gmail.com>
2022-09-08 17:52:29 +0300

    log memory of parent process and children with ps

---
+++ cpu_memory_stats/ps_newApproach.sh
@@ -0,0 +1,14 @@
+#a shell script to write CPU and Memory usage for a single process, derived from the ps command
+#Execution: ./ps_process_stats.sh <process_name> <path_to_output_file> 
+#!/bin/bash
+
+PNAME="$1"
+LOG_FILE="$2"
+
+
+while true ; do
+   #echo "$(date),$PNAME[$(pidof ${PNAME})] $(ps -C ${PNAME} -o %cpu -o %mem | tail -1)%" >>
+    echo "$(date)"," $(ps -C ${PNAME} -o rss,%mem,%cpu,pid,command)" >> $LOG_FILE
+    sleep 1
+done
+ 
